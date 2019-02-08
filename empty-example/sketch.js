f8b3b78e45724d0dc99384e77c1552871ff15d59
Lawrence Jiang <lawrencejiang@berkeley.edu>
2019-02-07 22:02:08 -0800

    expected resource per turn

--- empty-example/sketch.js
+++ empty-example/sketch.js
@@ -1,14 +1,32 @@
-var diceVals = [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
-var rectangles = [];
-var x_pos = 30;
-var y_pos = 200;
-var width = 60; 
-var height = 60;
-
+var resourceRows = [];
+var numResources = 5;
+var minDiceVal = 2;
+var maxDiceVal = 12;
+var resourceNames = ["Wood", "Brick", "Wheat", "Ore", "Sheep"];
+var expectedTurns = [0, 0, 0, 0, 0];
 
 function setup() {
   textAlign(CENTER);
   createCanvas(screen.width, screen.height);
+  background("#CC3333");
+
+  var diceValYPos = 150;
+  for (var i = 0; i < numResources; i++) {
+    var rollVals = [];
+    var diceValXPos = 150;
+    for (var val = minDiceVal; val <= maxDiceVal; val++) {
+      if (val == 7) {
+        continue;
+      }
+      rollVals.push(new diceVal(diceValXPos, diceValYPos, val));
+      diceValXPos += 65;
+    }
+    resourceRows.push(rollVals)
+    diceValYPos += 125;
+  }
+}
+
+function draw() {
   background("#CC3333");
 
   fill(239, 230, 59);
@@ -19,37 +37,100 @@ function setup() {
   textSize(24);
   text("Resource Information", windowWidth / 2, 100);
 
-  for (var i = 0; i < diceVals.length; i++) {
-    var diceVal = diceVals[i];
+  textSize(16);
+  text("Expected Resource per Turn", 1000, 120);
 
-    rectangles[i] = new Rectangle(x_pos, y_pos, width, height, diceVal, i);
-
-    x_pos = x_pos + width + 5;
+  var resourceNameXPos = 70;
+  var resourceNameYPos = 180;
+  for (var i = 0; i < resourceNames.length; i++) {
+    textSize(16);
+    text(resourceNames[i], resourceNameXPos, resourceNameYPos)
+    resourceNameYPos += 125;
   }
 
-  displayArray();
-
-
-  // text("Wood", windowWidth / 10, 150);
-
-  // text("Brick", (windowWidth / 10) * 3, 150);
-
-  // text("Wheat", (windowWidth / 10) * 5, 150);
-
-  // text("Ore", (windowWidth / 10) * 7, 150);
-
-  // text("Sheep", (windowWidth / 10) * 9, 150);
-
+  displayResources();
+  displayTurnsPerResource();
 }
 
-function displayArray() {
-  // displayBackgroundBox(145, rectangles[0].y + 100);
-  for(var i = 0; i < rectangles.length; i++) {
-    rectangles[i].display();
+function displayResources() {
+  for (var i = 0; i < resourceRows.length; i++) {
+    for (var j = 0; j < resourceRows[0].length; j++) {
+      resourceRows[i][j].display();
+    }
   }
 }
 
-function draw() {
+function displayTurnsPerResource() {
+  var resourceNameXPos = 1000;
+  var resourceNameYPos = 180;
+  for (var i = 0; i < resourceRows.length; i++) {
+    textSize(16);
+    var expectedTurns = 0;
+    for (var j = 0; j < resourceRows[0].length; j++) {
+      var roll = resourceRows[i][j].val;
+      var numPoints = resourceRows[i][j].numPoints;
+      if (roll == 2 || roll == 12) {
+        expectedTurns += numPoints * (1 / 36);
+      } else if (roll == 3 || roll == 11) {
+        expectedTurns += numPoints * (2 / 36);
+      } else if (roll == 4 || roll == 10) {
+        expectedTurns += numPoints * (3 / 36);
+      } else if (roll == 5 || roll == 9) {
+        expectedTurns += numPoints * (4 / 36);
+      } else if (roll == 6 || roll == 8) {
+        expectedTurns += numPoints * (5 / 36);
+      }
+    }
 
+    expectedTurns = expectedTurns.toFixed(2);
 
+    text(expectedTurns, resourceNameXPos, resourceNameYPos);
+    resourceNameYPos += 125;
+  }
+}
+
+function mousePressed() {
+  for(var i = 0; i < resourceRows.length; i++) {
+    for(var j = 0; j < resourceRows[0].length; j++) {
+      resourceRows[i][j].clicked();
+    }
+  }
+}
+
+function diceVal(xPos, yPos, val) {
+  this.x = xPos;
+  this.y = yPos;
+  this.val = val;
+  this.numPoints = 0;
+
+  this.display = function() {
+    stroke(255);
+    fill(255, (1 / Math.abs(this.val - 7)) * 175);
+    // Display box
+    rect(this.x, this.y, 50, 50);
+
+    // Display dice value
+    textSize(16);
+    text(this.val, this.x + 25, this.y - 10);
+    // Display num points
+    textSize(16 + this.numPoints * 2);
+    text(this.numPoints, this.x + 25, this.y + 30 + this.numPoints);
+
+    // Display + - boxes
+    textSize(16);
+    square(this.x, this.y + 50, 16);
+    text("-", this.x + 8, this.y + 63);
+    square(this.x + 34, this.y + 50, 16);
+    text("+", this.x + 43, this.y + 63);
+  }
+
+  this.clicked = function() {
+    if (this.x <= mouseX && mouseX <= this.x + 16 && this.y + 50 <= mouseY && mouseY <= this.y + 66) {
+      if (this.numPoints > 0) {
+        this.numPoints -= 1;
+      }
+    } else if (this.x + 34 <= mouseX && mouseX <= this.x + 50 && this.y + 50 <= mouseY && mouseY <= this.y + 66) {
+      this.numPoints += 1;
+    }
+  }
 }
